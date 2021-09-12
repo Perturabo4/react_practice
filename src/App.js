4546bb8f16e34f6d57cd96ffff70f545a661dd5e
Viktor <perturabo40@gmail.com>
2021-09-12 20:16:56 +0300

    second part

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import { useState } from "react";
+import PostList from "./components/PostList.js";
 
 function App() {
 
-  const [value, setValue] = useState('text')
+  const [posts, setPosts] = useState([
+    { id: 1, name: 'JS', body: 'JS is a programming language' },
+    { id: 2, name: 'JS', body: 'JS is a programming language 2' }
+  ])
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            Js is a programming language
-          </div>
-        </div>
-        <div className="post_btns">
-            <button>Delete</button>
-          </div>
-      </div>
+      <PostList posts={posts} title='Post list 1' />
     </div>
   );
 }
